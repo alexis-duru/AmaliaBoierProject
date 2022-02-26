4c659757e6fdfd5cd166d58e4aac8a54fca28acd
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2022-02-26 19:30:17 +0100

    AmaliaBoier Project

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+function menuToggle() {
+    var nav = document.getElementById("nav")
+    var toggle = document.getElementById("toggle")
+    nav.classList.toggle("active")
+    toggle.classList.toggle("active")
+}
+
+function myFunction() {
+    var changeText = document.getElementById("changeMenu");
+    if (changeText.innerHTML === "Close") {
+        changeText.innerHTML = "Menu";
+    } else {
+        changeText.innerHTML = "Close";
+    }
+  }
+
